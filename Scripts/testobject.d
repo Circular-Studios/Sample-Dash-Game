f3159aa0700b58f7eecd6ccb3148a9f011000105
Colden Cullen <ColdenCullen@gmail.com>
2014-02-21 20:27:57 -0500

    Wrote code to test input bindings.

--- Scripts/testobject.d
+++ Scripts/testobject.d
@@ -1,12 +1,24 @@
 module testobject;
 import core.gameobject;
-import utility.output, utility.time;
+import utility.input, utility.output, utility.time;
 
 class TestObject : GameObject
 {
 	// Overridables
 	override void onUpdate()
 	{
+		if( Input.getState( "Forward" ) )
+		{
+			log( OutputType.Info, "Forward" );
+		}
+		if( Input.getState( "Backward" ) )
+		{
+			log( OutputType.Info, "Backward" );
+		}
+		if( Input.getState( "Jump" ) )
+		{
+			log( OutputType.Info, "Jump" );
+		}
 		this.transform.rotation.rotatey( std.math.PI / 130 * Time.deltaTime);
 		this.transform.rotation.rotatez( std.math.PI / -120 * Time.deltaTime);
 		this.transform.updateMatrix();
