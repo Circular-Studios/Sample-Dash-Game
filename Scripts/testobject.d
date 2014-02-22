9c5483fb601d942f1d6f07e8a81ac922fe6a3a3a
Brandon Littell <brandonl3737@gmail.com>
2014-02-22 17:04:44 -0500

    Added some comments to testobject.d

--- Scripts/testobject.d
+++ Scripts/testobject.d
@@ -22,28 +22,29 @@ class TestObject : GameObject
 		{
 			log( OutputType.Info, "Jump" );
 		}
-		//this.transform.rotation.rotatey( std.math.PI / 130 * Time.deltaTime);
-		//this.transform.rotation.rotatez( std.math.PI / -120 * Time.deltaTime);
+
 		this.transform.updateMatrix();
 
 		auto liiiiight = (cast(DirectionalLight)this.light);
+
+		// If you were actually thinking about doing this in your game, please don't.
 		static float red = 0.0f;
 		static float blu = 50.0f;
 		static float gre = 100.0f;
 		static float redMod = 0.1f;
 		static float bluMod = 0.1f;
 		static float greMod = 0.1f;
+
+		// Change color direction
 		if(red >= 100 || red < 0) redMod = -redMod;
 		if(blu >= 100 || blu < 0) bluMod = -bluMod;
 		if(gre >= 100 || gre < 0) greMod = -greMod;
-
+	
 		red += redMod;
 		blu += bluMod;
 		gre += greMod;
-		//log(OutputType.Info, "Color", color );
-		//liiiiight.color.x += color;
+
 		liiiiight.color = vec3(red/100.0f, blu/100.0f, gre/100.0f);
-		//liiiiight.color = vec3(liiiiight.color.x + color, liiiiight.color.x + color2, liiiiight.color.x + color3);
 	}
 
 	/// Called on the draw cycle.
