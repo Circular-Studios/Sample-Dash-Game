0e4c3d4b5631aca6477013a01e40867873d4b3da
Eric Christenson <esc6491@rit.edu>
2014-04-28 16:10:20 -0400

    Cleaned up garbage in testobject, added additional planes

--- Scripts/testobject.d
+++ Scripts/testobject.d
@@ -33,28 +33,6 @@ shared class TestObject : Behavior!TOArgs
         {
             logNotice( "Jump" );
         }
-
-        /*
-        auto liiiiight = (cast(DirectionalLight)this.light);
-
-        // If you were actually thinking about doing this in your game, please don't.
-        static float red = 0.0f;
-        static float blu = 50.0f;
-        static float gre = 100.0f;
-        static float redMod = 0.1f;
-        static float bluMod = 0.1f;
-        static float greMod = 0.1f;
-
-        // Change color direction
-        if(red >= 100 || red < 0) redMod = -redMod;
-        if(blu >= 100 || blu < 0) bluMod = -bluMod;
-        if(gre >= 100 || gre < 0) greMod = -greMod;
-    
-        red += redMod;
-        blu += bluMod;
-        gre += greMod;
-
-        liiiiight.color = vec3(red/100.0f, blu/100.0f, gre/100.0f); */
     }
 
     /// Called on the draw cycle.
@@ -62,27 +40,6 @@ shared class TestObject : Behavior!TOArgs
     /// Called on shutdown.
     override void onShutdown() { }
 }
-
-shared class MovePointLight : Behavior!()
-{
-    // Overridables
-    override void onUpdate()
-    {
-        static float t = 0.0;
-        t += std.math.PI/2 * Time.deltaTime;
-    //  this.transform.position = vec3( 20*cos(t), 20*sin(t), this.transform.position.z );
-        //this.transform.position.x = 10*cos(t);
-        //this.transform.position.y = 10*sin(t);
-
-        //this.transform.rotation.rotatez( -std.math.PI * Time.deltaTime);
-    }
-
-    /// Called on the draw cycle.
-    override void onDraw() { }
-    /// Called on shutdown.
-    override void onShutdown() { }
-}
-
 
 shared class RotateThing : Behavior!()
 {
@@ -104,8 +61,6 @@ shared class RotateThing : Behavior!()
         }
 
         this.transform.rotation.rotatey( -std.math.PI * Time.deltaTime );
-        //(cast()this.transform.rotation).rotatey( std.math.PI * Time.deltaTime);
-        //(cast()this.transform.rotation).rotatex( -std.math.PI * Time.deltaTime);
     }
     
     /// Called on the draw cycle.
